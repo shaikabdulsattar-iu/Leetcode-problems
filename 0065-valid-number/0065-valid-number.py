beae6bb37a009ee5ba6c7869637d8be2d582f01e
Shaikabdul sattar <shaikabdulsattar663@gmail.com>
2026-07-20 18:24:59 +0530

    Time: 0 ms (100%), Space: 19.3 MB (39.02%) - LeetHub

---
+++ 0065-valid-number/0065-valid-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        try:
+            return (float(s) or int(float(s)) == 0) and not ('n'in s)
+        except:
+            return(False) 
+        
