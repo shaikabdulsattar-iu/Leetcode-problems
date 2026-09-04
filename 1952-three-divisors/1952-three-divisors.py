d1211dede7f4abb13390ccd313a2ac7efdf3d6be
Shaikabdul sattar <shaikabdulsattar663@gmail.com>
2026-09-04 14:28:16 +0530

    Time: 3 ms (62.13%), Space: 19.2 MB (84.26%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.py
+++ 1952-three-divisors/1952-three-divisors.py
@@ -4,8 +4,5 @@ class Solution:
         for i in range(1,n+1):
             if n % i == 0:
                 c = c + 1
-        if c == 3:
-            return True
-        else:
-            return False            
+        return c == 3           
         
