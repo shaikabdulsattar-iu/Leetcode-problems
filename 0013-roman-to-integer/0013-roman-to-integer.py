3a1349b32c83a11daeaba55a0a4d38d8f144577e
Shaikabdul sattar <shaikabdulsattar663@gmail.com>
2026-09-18 10:42:45 +0530

    Time: 7 ms (41.21%), Space: 19 MB (99.73%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,22 +1,41 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_map = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-        
-        total = 0
-        n = len(s)
-        
-        for i in range(n):
-            if i + 1 < n and roman_map[s[i]] < roman_map[s[i + 1]]:
-                total -= roman_map[s[i]]
-            else:
-                total += roman_map[s[i]]
-                
-        return total
+        s = (
+            s.replace("IV", "a")
+            .replace("IX", "b")
+            .replace("XL", "c")
+            .replace("XC", "d")
+            .replace("CD", "e")
+            .replace("CM", "f")
+        )
+
+        sum_ = 0
+        for i in s:
+            if i == "a":
+                sum_ += 4
+            elif i == "b":
+                sum_ += 9
+            elif i == "c":
+                sum_ += 40
+            elif i == "d":
+                sum_ += 90
+            elif i == "e":
+                sum_ += 400
+            elif i == "f":
+                sum_ += 900
+            elif i == "I":
+                sum_ += 1
+            elif i == "V":
+                sum_ += 5
+            elif i == "X":
+                sum_ += 10
+            elif i == "L":
+                sum_ += 50
+            elif i == "C":
+                sum_ += 100
+            elif i == "D":
+                sum_ += 500
+            elif i == "M":
+                sum_ += 1000
+
+        return sum_
